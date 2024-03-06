f4c96090aeb09bbe718a7afe08c5f70e03f1c7ee
DevMasterSh <dev_master_sh@mail.ru>
2024-03-07 01:23:19 +0300

    Added booking form

--- js/script.js
+++ js/script.js
@@ -109,3 +109,39 @@ galleryRoomTwoArrows.addEventListener('click', (e) => {
         galleryRoomTwoImage.alt = roomStandartTwo[galleryRoomTwoImageIndex - 1].alt;
     }
 });
+
+/* Modal window */
+const dialog = document.getElementById('myDialog')
+const dialogOpener = document.querySelectorAll('.booking')
+const dialogCloser = document.querySelector('.closeDialogBtn')
+
+function closeOnBackDropClick({ currentTarget, target }) {
+    const dialog = currentTarget
+    const isClickedOnBackDrop = target === dialog
+    if (isClickedOnBackDrop) {
+        dialog.close()
+    }
+}
+
+function openModalAndLockScroll() {
+    dialog.showModal()
+    document.body.classList.add('scroll-lock')
+}
+
+function returnScroll() {
+    document.body.classList.remove('scroll-lock')
+}
+
+function close() {
+    dialog.close()
+    returnScroll()
+}
+
+dialog.addEventListener('click', closeOnBackDropClick)
+dialogOpener.forEach((buttonOpener) => {
+    buttonOpener.addEventListener('click', openModalAndLockScroll)
+})
+dialogCloser.addEventListener('click', (event) => {
+    event.stopPropagation()
+    close()
+})
